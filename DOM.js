2e4f562db7d093626006bd0e740635bcfd467936
Siri-Varshini-07 <sirivarshini07@gmail.com>
2025-06-25 15:06:46 +0530

    dom

---
+++ DOM.js
@@ -0,0 +1,25 @@
+document.title="Dom in javascript";
+
+document.getElementById("header").innerHTML="Hello  World";
+
+document.getElementById("para").style.fontSize="30px";
+document.getElementById("para").style.color="blue";
+document.getElementById("para").style.fontFamily="sans-serif";
+document.getElementById("para").style.textAlign="center";
+
+document.getElementById("Parrot").style.color="red";
+
+document.body.style.backgroundColor="cyan";
+
+document.getElementById("pastel").style.backgroundColor="pink";
+document.getElementById("pastel").style.width="370px";
+document.getElementById("pastel").style.height="40px";
+
+
+document.getElementsByClassName("phone")[0].style.color="yellow";
+document.getElementsByClassName("phone")[1].style.color="green";
+document.getElementsByClassName("phone")[2].style.color="grey";
+document.getElementsByClassName("phone")[3].style.color="white";
+
+document.getElementById("dimple").style.width="100px";
+document.getElementById("dimple").style.height="190px";
