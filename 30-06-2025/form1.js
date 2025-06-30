5bf55437202913d75fdd9897512cfecad2ce9a09
Siri-Varshini-07 <sirivarshini07@gmail.com>
2025-06-30 21:07:46 +0530

    Add files via upload

---
+++ 30-06-2025/form1.js
@@ -0,0 +1,14 @@
+/* let aish=document.createElement("h1");
+aish.textContent="STUDENT INFORMATION FORM";
+/* aish.innerHTML="Memory 1 Tera Byte"; */
+/* aish.style.color="blue";
+aish.style.fontSize="20px"; 
+document.getElementsByClassName("student").appendChild(aish); */ 
+
+let form=document.getElementById("myform");
+        let input=document.getElementById("textInput");
+        form.addEventListener("reset", function(){
+            input.value=null;
+        });
+
+document.getElementById("name");
