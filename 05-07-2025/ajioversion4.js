a55448ba12c2f627dc27027a08a7353c6957d8c0
Siri-Varshini-07 <sirivarshini07@gmail.com>
2025-07-05 15:05:50 +0530

    Add files via upload

---
+++ 05-07-2025/ajioversion4.js
@@ -0,0 +1,90 @@
+let cartCount = 0;
+document.querySelectorAll("button").forEach(btn => {
+    if (btn.innerText === "Buy now") {
+        btn.addEventListener("click", function () {
+            cartCount++;
+            document.getElementById("cart-count").innerText = cartCount;
+        });
+    }
+}); 
+
+document.getElementById("link").addEventListener("click",function(){
+    alert("You are being redirected.");
+});
+
+
+document.getElementById("dark-theme").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+});
+
+function myfunction() {
+    const button = document.getElementById("select");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction1() {
+    const button = document.getElementById("select1");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction2() {
+    const button = document.getElementById("select2");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction3() {
+    const button = document.getElementById("select3");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction4() {
+    const button = document.getElementById("select4");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction5() {
+    const button = document.getElementById("select5");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction6() {
+    const button = document.getElementById("select6");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
+
+function myfunction7() {
+    const button = document.getElementById("select7");
+    if (button.innerHTML === "♡") {
+        button.innerHTML = "❤️"; 
+    } else {
+        button.innerHTML = "♡"; 
+    }
+}
